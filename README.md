afa312899a5aed411d94621aa9a78b82fc3b48af
felipetavaresmelo <felipetavaresmelo@gmail.com>
2022-03-27 11:28:08 -0300

    H2 database, test profile, JPA

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# course
+# Course
 
 Projeto do curso de Nélio Alves.
 
-Java COMPLETO Programação Orientada a Objetos +Projetos
+### Java COMPLETO Programação Orientada a Objetos +Projetos
+
 https://www.udemy.com/course/java-curso-completo/
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,8 +2,13 @@ package com.educandoweb.course.entities;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -12,6 +17,9 @@ import java.io.Serializable;
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include private Long id;
     private String name;
     private String email;
